66b0dd7ea57343db16a2d7fe0b4b27dceb02a514
angela.soler <angela.soler@igs-software.com.br>
2024-01-20 22:28:34 -0300

    utils para texturas

--- includes/libft.h
+++ includes/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tasantos <tasantos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:01:26 by asoler            #+#    #+#             */
-/*   Updated: 2024/01/19 23:57:19 by tasantos         ###   ########.fr       */
+/*   Updated: 2024/01/14 20:31:04 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,5 +113,7 @@ int		ft_toupper(int c);
 char	*get_next_line(int fd);
 
 int		ft_printf(const char *s, ...);
+
+int		ft_hex_to_int(char *n_hex);
 
 #endif
